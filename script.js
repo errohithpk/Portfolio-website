8ee42c1f2f6766247cc8ad7b5ac341dbf6aec8a0
errohithpk <er.rohithpk@gmail.com>
2026-03-15 22:46:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(sec=>{
+
+const top = window.scrollY;
+const offset = sec.offsetTop - 300;
+const height = sec.offsetHeight;
+
+if(top >= offset && top < offset + height){
+
+sec.classList.add("active");
+
+}
+
+})
+
+})
